4814a79a15a1f7f2b889fdc97f634d0b01938678
cnbs5 <879700945@qq.com>
2018-03-21 17:41:26 +0800

    hello world

--- js/main/MainScreen.js
+++ js/main/MainScreen.js
@@ -1,58 +1,148 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+"use strict";
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     Platform,
     StyleSheet,
     Text,
-    View
+    View,
+    Image,
+    Colors,
 } from 'react-native';
 
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-    android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {StackNavigator, TabNavigator} from 'react-navigation';
 
-type Props = {};
-export default class MainScreen extends Component<Props> {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit App.js
-                </Text>
-                <Text style={styles.instructions}>
-                    {instructions}
-                </Text>
-            </View>
-        );
-    }
-}
+import NewsScreen from '../news/NewsScreen'
+import CardsScreen from '../cards/CardsScreen'
+import MyScreen from '../my/MyScreen'
+
+
+import BindPhoneScreen from '../my/phone/BindPhoneScreen'
+
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+    icon: {
+        height: 20,
+        width: 20,
     },
 });
+
+const MainTabNavigator = TabNavigator({
+    News: {
+        screen: NewsScreen,
+        navigationOptions: {
+            headerTitle: '线上特惠',
+            tabBarLabel: '线上特惠',
+            headerLeft: null,//去左边返回键
+            headerStyle: {
+                backgroundColor: '#03A7FF',
+            },
+            headerTitleStyle: {
+                color: 'white'
+            },
+            tabBarIcon: ({tintColor}) => (
+                <Image
+                    source={require('../news/img/news.png')}
+                    style={[styles.icon, {tintColor: tintColor}]}
+                />
+            ),
+        },
+
+    },
+    Cards: {
+        screen: CardsScreen,
+        navigationOptions: {
+            // header: ,
+            headerTitle: '金融产品',
+            tabBarLabel: '金融产品',
+            headerLeft: null,
+            headerStyle: {
+                backgroundColor: '#03A7FF',
+            },
+            headerTitleStyle: {
+                color: 'white'
+            },
+
+            tabBarIcon: ({tintColor}) => (
+                <Image
+                    source={require('../cards/img/cards.png')}
+                    style={[styles.icon, {tintColor: tintColor}]}
+                />
+            ),
+        },
+    },
+    My: {
+        screen: MyScreen,
+        navigationOptions: {
+            headerTitle: '我的',
+            tabBarLabel: '我的',
+            headerLeft: null,//去左边返回键
+            headerStyle: {
+                backgroundColor: '#03A7FF',
+            },
+            headerTitleStyle: {
+                color: 'white'
+            },
+
+            tabBarIcon: ({tintColor}) => (
+                <Image
+                    source={require('../my/img/my.png')}
+                    style={[styles.icon, {tintColor: tintColor}]}
+                />
+            ),
+        },
+    },
+}, {
+    tabBarPosition: 'bottom',
+    animationEnabled: false,
+    swipeEnabled: false,
+    tabBarOptions: {
+        activeTintColor: '#46bafe',
+        inactiveTintColor: '#6a7084',
+        showLabel: true,
+        showIcon: true,
+        style: {
+            backgroundColor: 'white',
+            height: 60
+        },
+        indicatorStyle: {
+            height: 0
+        }
+    }
+});
+
+const MyStackNavigator = StackNavigator({
+    BindPhoneScreen: {
+        screen: BindPhoneScreen,
+        navigationOptions: {
+            headerTitle: '手机号绑定',
+        }
+    },
+}, {
+    navigationOptions: {},
+    headerMode: 'none',
+})
+
+const MainStackNavigator = StackNavigator({
+    MainTabNavigator: {
+        screen: MainTabNavigator,
+    },
+    // HomeDetailScreen: {
+    //     screen: HomeDetailScreen,
+    //     navigationOptions: {
+    //         headerTitle: '详情',
+    //     }
+    // },
+    // PractiseNavi: {
+    //     screen: PractiseNavi,
+    // },
+    MyStackNavigator: {
+        screen: MyStackNavigator,
+    },
+
+}, {
+    navigationOptions: {},
+    // headerMode: 'none',
+})
+
+export default MainStackNavigator
---
+++ js/my/MyScreen.js
@@ -0,0 +1,59 @@
+"use strict";
+
+import React, {Component} from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+} from 'react-native';
+
+import BindPhoneScreen from './phone/BindPhoneScreen'
+
+type Props = {};
+export default class App extends Component<Props> {
+
+    _goSetPhone = () => {
+        this.props.navigation.navigate('BindPhoneScreen', {
+            // newsId: item.newsId,
+        })
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity onPress={this._goSetPhone.bind(this)}>
+                    <View style={styles.phoneParent}>
+                        <Text>手机号：</Text>
+                        <Text>未绑定手机号</Text>
+
+                        <View style={{flex: 1, flexDirection: 'row-reverse'}}>
+                            <Image
+                                source={require('./img/arrow.png')}
+                                style={styles.arrow}/>
+                        </View>
+
+                    </View>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+    },
+    phoneParent: {
+        flex: 1,
+        flexDirection: 'row',
+        padding: 24
+    },
+    arrow: {
+        height: 20,
+        width: 20
+    },
+});
