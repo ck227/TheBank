9dd6898da63d1986b6eb44d08c99e711fa895a98
cnbs5 <879700945@qq.com>
2018-04-02 18:02:50 +0800

    hey

--- js/network/constants.js
+++ js/network/constants.js
@@ -8,11 +8,15 @@ export const constants = {
 
 /**
  *
+ * 后台地址： http://183.95.84.193:8089/zxtFinancial/loginAct/zxt_login.html
+ * 模拟线上： http://183.95.84.193:8089/zxtFinancial/
+ *
  * 1.线上特惠暂无数据界面 (好了)
  * 2.金融产品暂无数据界面 （好了）
  * 3.卡片详情界面
- * 4.申请卡片
- * 5.绑定/接触绑定手机
+ * http://192.168.100.222:8080/zxtFinancial/bankcardruleFront/isSubmit.html?userId=1&bankCardId=1   判断是否提交过申请
+ * 4.申请卡片    http://192.168.100.222:8080/zxtFinancial/bankcardruleFront/listBean.html?bankCardId=1
+ * 5.绑定/解除绑定手机
  * 6.介绍记录
  * 7.申请记录
  * 8。IOS兼容  （慢慢来）
